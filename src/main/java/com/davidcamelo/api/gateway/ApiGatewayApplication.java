5ae650e5f2a23fa78265abccbd8e121951dcf043
David Camelo <dacaro16@gmail.com>
2025-04-22 18:38:01 -0500

    Setup API Gateway

---
+++ src/main/java/com/davidcamelo/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.davidcamelo.api.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		log.info("Current java.home {}", System.getProperty("java.home"));
+		log.info("Current java.vendor {}", System.getProperty("java.vendor"));
+		log.info("Current java.vendor.url {}", System.getProperty("java.vendor.url"));
+		log.info("Current java.version {}", System.getProperty("java.version"));
+
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
---
+++ src/main/java/com/davidcamelo/api/gateway/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.davidcamelo.api.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.HashSet;
+
+import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+    private final RouteDefinitionLocator locator;
+    private final SwaggerUiConfigProperties swaggerUiConfigProperties;
+
+    @Scheduled(fixedDelay = 60_000)
+    public void updateSwaggerDocs() {
+        var definitions = locator.getRouteDefinitions().collectList().block();
+        var urls = new HashSet<AbstractSwaggerUiConfigProperties.SwaggerUrl>();
+        assert definitions != null;
+        definitions.stream()
+                .filter(routeDefinition -> (
+                        routeDefinition.getId().startsWith("ReactiveCompositeDiscoveryClient")
+                        && !routeDefinition.getId().endsWith("API-GATEWAY")
+                        && !routeDefinition.getId().endsWith("CONFIG-SERVER"))
+                )
+                .forEach(routeDefinition -> {
+                    var name = routeDefinition.getId().substring(routeDefinition.getId().lastIndexOf("_") + 1).toLowerCase();
+                    var swaggerUrl = new AbstractSwaggerUiConfigProperties.SwaggerUrl(name, DEFAULT_API_DOCS_URL + "/" + name, null);
+                    urls.add(swaggerUrl);
+                });
+        swaggerUiConfigProperties.setUrls(urls);
+    }
+}
